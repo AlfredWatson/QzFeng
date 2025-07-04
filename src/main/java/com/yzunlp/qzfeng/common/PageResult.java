b593c40d62b5d5b4de70f3450f2b980e6c2bef22
huang <9624219@qq.com>
2025-07-04 18:06:53 +0800

    todo

---
+++ src/main/java/com/yzunlp/qzfeng/common/PageResult.java
@@ -0,0 +1,26 @@
+package com.yzunlp.qzfeng.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * @author: Huang
+ * @description: TODO
+ * @date: 2025/7/4 17:57
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PageResult implements Serializable {
+
+    private long total; //总记录数
+
+    private List records; //当前页数据集合
+
+}
+
---
+++ src/main/java/com/yzunlp/qzfeng/mapper/AdminMapper.java
@@ -0,0 +1,28 @@
+package com.yzunlp.qzfeng.mapper;
+
+import com.github.pagehelper.Page;
+import com.yzunlp.qzfeng.domain.po.UserCheckupForm;
+import com.yzunlp.qzfeng.domain.po.UserEval;
+import com.yzunlp.qzfeng.domain.po.UserPropolis;
+import com.yzunlp.qzfeng.domain.vo.UserInfoBaseVO;
+import com.yzunlp.qzfeng.domain.vo.UserInfoHealthVO;
+import com.yzunlp.qzfeng.domain.vo.userQuestionnaireVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface AdminMapper {
+
+
+    UserInfoHealthVO selectUserInfoHealthById(Long id);
+
+
+    List<UserPropolis> selectUserPropolisByUserId(Long id);
+
+    List<UserEval> selectUserEvalByUserId(Long id);
+
+    List<UserCheckupForm> selectUserCheckupFormByUserId(Long id);
+
+    Page<UserInfoBaseVO> selectAllUsers(int pageNum, int pageSize);
+}
