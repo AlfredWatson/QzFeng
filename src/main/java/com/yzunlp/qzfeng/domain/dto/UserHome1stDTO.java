764e48ddea28da057932b6a30178b05c1e4d77c9
huang <9624219@qq.com>
2025-07-29 19:28:51 +0800

    test

--- src/main/java/com/yzunlp/qzfeng/domain/dto/UserHome1stDTO.java
+++ src/main/java/com/yzunlp/qzfeng/domain/dto/UserHome1stDTO.java
@@ -24,13 +24,13 @@ public class UserHome1stDTO {
 
     private Boolean hypertension; // 是否有高血压
     private Integer hypertensionYear; // 高血压病程几年
-    private Boolean hypertensionDrug; // 是否规律服药
+    private boolean hypertensionDrug; // 是否规律服药
     private Boolean diabetes; // 是否有糖尿病
     private Integer diabetesYear; // 糖尿病病程几年
-    private Boolean diabetesDrug; // 是否规律服药
+    private boolean diabetesDrug; // 是否规律服药
     private Boolean hyperlipidemia; // 是否有高血脂
     private Integer hyperlipidemiaYear; // 高血脂病程几年
-    private Boolean hyperlipidemiaDrug; // 是否规律服药
+    private boolean hyperlipidemiaDrug; // 是否规律服药
     private Boolean tumor; // 是否有肿瘤
 
     private Boolean propolis; // 是否服用过蜂胶
--- src/main/java/com/yzunlp/qzfeng/domain/po/UserHealth.java
+++ src/main/java/com/yzunlp/qzfeng/domain/po/UserHealth.java
@@ -17,15 +17,15 @@ public class UserHealth implements Serializable {
 
     private Boolean hypertension; // 是否有高血压
     private Integer hypertensionYear; // 高血压病程几年
-    private Boolean hypertensionDrug; // 是否规律服药
+    private Boolean hypertensionDrug=false; // 是否规律服药
 
     private Boolean diabetes; // 是否有糖尿病
     private Integer diabetesYear; // 糖尿病病程几年
-    private Boolean diabetesDrug; // 是否规律服药
+    private Boolean diabetesDrug =false; // 是否规律服药
 
     private Boolean hyperlipidemia; // 是否有高血脂
     private Integer hyperlipidemiaYear; // 高血脂病程几年
-    private Boolean hyperlipidemiaDrug; // 是否规律服药
+    private Boolean hyperlipidemiaDrug=false ; // 是否规律服药
 
     private Boolean tumor; // 是否有肿瘤
 }
